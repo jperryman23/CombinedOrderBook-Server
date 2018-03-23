223f42804ef382d365e6930cd33af8545e476a7e
Jules Perryman <jules.perryman@gmail.com>
2018-03-23 12:15:43 -0600

    updated knex file

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-// require('dotenv').config()
+require('dotenv').config()
 const path = require('path');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
--- db/knex.js
+++ db/knex.js
@@ -1,18 +1,17 @@
 // const environment = process.env.NODE_ENV || 'development' ;
 // const config = require('../knexfile')[environment]
 // // const environmentConfig = config[environment]
-// // const knex = require('knex')
+// const knex = require('knex')('production')
 // // const connection = knex(environmentConfig);
 //
 // const pg = require('knex')(config[environment])
 //
 // // module.exports = require('knex')(config)
 // module.exports = pg;
-
-
+const knex = require('knex')
 const config = require('../knexfile')
 
-const environment = process.env.NODE_ENV || 	'development'
+const environment = process.env.NODE_ENV ||'development'
 
 const pg = require('knex')(config[environment])
 
